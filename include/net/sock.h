aa48b12f379e94d6c9e6a53da9a76c51769f18bc
KyuoFoxHuyu <88403766@qq.com>
2022-04-04 09:31:00 +0800

    net: Import xiaomi changes
    Signed-off-by: KyuoFoxHuyu <88403766@qq.com>

--- include/net/sock.h
+++ include/net/sock.h
@@ -421,6 +421,7 @@ struct sock {
 	__u32			sk_priority;
 	__u32			sk_mark;
 	kuid_t			sk_uid;
+	pid_t			pid_num;
 	spinlock_t		sk_peer_lock;
 	struct pid		*sk_peer_pid;
 	const struct cred	*sk_peer_cred;
